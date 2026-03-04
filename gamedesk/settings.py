fa610fde1b99f99b404d69bb805cb3cb43a35c3d
shubham mahyavanshi <shubhammahyavanshi75@gmail.com>
2026-03-04 16:41:13 +0530

    fix static render

--- gamedesk/settings.py
+++ gamedesk/settings.py
@@ -106,6 +106,9 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 # WhiteNoise static compression
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
